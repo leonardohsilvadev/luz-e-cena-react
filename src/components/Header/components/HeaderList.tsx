86a4c0e57625eb22ab7cfe4e6a7a45375f9691ce
Leonardo Silva <leonardohsilva.dev@gmail.com>
2025-11-18 16:55:14 -0300

    feat: create Header component w/ HeaderList n HeaderListItem components

---
+++ src/components/Header/components/HeaderList.tsx
@@ -0,0 +1,7 @@
+import styles from "../Header.module.css";
+
+const HeaderList = ({ children }: React.HTMLAttributes<HTMLUListElement>) => {
+  return <ul className={styles.cabecalho}>{children}</ul>;
+};
+
+export default HeaderList;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Banner } from "./Banner";
 export { default as Link } from "./Link";
 export { default as Button } from "./Button";
+export { default as Header } from "./Header";
